fc3d1b745c2e606bfdca670ebfc28ea470e2530b
Chris Lamb <chris@playfire.com>
2012-04-10 15:08:45 +0100

    Style fixups.

--- django_autologin/templatetags/django_autologin.py
+++ django_autologin/templatetags/django_autologin.py
@@ -1,14 +1,15 @@
 from django import template
 from django.core.signing import TimestampSigner
 
-from .. import app_settings, utils
+from .. import app_settings
+from ..utils import get_user_salt
 
 register = template.Library()
 
 @register.simple_tag
 def automatic_login_token(user):
     token = TimestampSigner(
-        salt=utils.get_user_salt(user),
+        salt=get_user_salt(user),
     ).sign(user.id)
 
     return "%s=%s" % (app_settings.KEY, token)
