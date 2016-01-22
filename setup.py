25fb09ccaf66a3a0d0f8931083c17c7e27ed1e4b
Chris Lamb <chris@chris-lamb.co.uk>
2016-01-22 11:37:31 +0100

    Releasing version 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='django-autologin',
 
     url="https://chris-lamb.co.uk/projects/django-slack",
-    version='0.0.1',
+    version='0.0.2',
     description="Token generator and processor to provide automatic login links for users",
 
     author="Chris Lamb",
