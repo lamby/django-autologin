adef8bca66a55a69dc19690d0845a0b42040c4b3
Chris Lamb <chris@chris-lamb.co.uk>
2016-01-22 11:52:59 +0100

    Releasing version 0.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='django-autologin',
 
     url="https://chris-lamb.co.uk/projects/django-autologin",
-    version='0.0.3',
+    version='0.0.4',
     description="Token generator and processor to provide automatic login links for users",
 
     author="Chris Lamb",
