b9681b4ab492a94f8e916c093e7cd92ad7d3af68
Dan Palmer <dan@thread.com>
2021-03-24 18:49:38 +0000

    Expose template tag logic as independent public API
    Sometimes it's useful to generate tokens in Python rather than in templates, but importing and running templatetags is a bit of a layering violation.

--- django_autologin/templatetags/django_autologin.py
+++ django_autologin/templatetags/django_autologin.py
@@ -1,14 +1,6 @@
 from django import template
-from django.core.signing import TimestampSigner
 
-from .. import app_settings
-from ..utils import get_user_salt
+from ..utils import get_automatic_login_token
 
 register = template.Library()
-
-
-@register.simple_tag
-def automatic_login_token(user):
-    token = TimestampSigner(salt=get_user_salt(user),).sign(user.pk)
-
-    return "%s=%s" % (app_settings.KEY, token)
+register.simple_tag(get_automatic_login_token, name="automatic_login_token")
